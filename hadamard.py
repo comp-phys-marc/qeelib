8d25c4f2b82ac6a068bbc7abdfb76f8c06be9d9c
Marcus Edwards <marcusedwards@hotmail.ca>
2019-02-14 13:04:39 -0500

    init commit

--- hadamard.py
+++ hadamard.py
@@ -3,18 +3,34 @@ from state import State
 from superimposed_states import States
 import math
 
-def hadamard(first_state, second_state, qubit):
-    first_coeff = Coefficient(magnitude=math.sqrt(0.25), imaginary=True)
+def hadamard_edge(first_state, second_state, qubit):
+    first_coeff = Coefficient(magnitude=1.00, imaginary=False)
     first_state = State(coeff=first_coeff, val=first_state)
     
-    second_real_coeff = Coefficient(magnitude=math.sqrt(0.75), imaginary=False)
-    second_im_coeff = Coefficient(magnitude=math.sqrt(0.75), imaginary=True)
-    second_coeff = ComplexCoefficient(real_component=second_real_coeff, imaginary_component=second_im_coeff)
+    second_coeff = Coefficient(magnitude=0.00, imaginary=False)
     second_state = State(coeff=second_coeff, val=second_state)
     
     state = States(state_array=[first_state, second_state], num_qubits=2)
     
     state.print()
     state.h(qubit)
+    
+def hadamard_chief(first_state, second_state, third_state, fourth_state, qubit):
+    first_coeff = Coefficient(magnitude=1.00, imaginary=False)
+    first_state = State(coeff=first_coeff, val=first_state)
+    
+    second_coeff = Coefficient(magnitude=0.00, imaginary=False)
+    second_state = State(coeff=second_coeff, val=second_state)
+    
+    third_coeff = Coefficient(magnitude=1.00, imaginary=False)
+    third_state = State(coeff=third_coeff, val=third_state)
+    
+    fourth_coeff = Coefficient(magnitude=0.00, imaginary=False)
+    fourth_state = State(coeff=fourth_coeff, val=fourth_state)
+    
+    state = States(state_array=[first_state, second_state, third_state, fourth_state], num_qubits=2)
+    
+    state.print()
+    state.h(qubit)
 
-hadamard("00", "11", 0)
+hadamard_chief("00", "01", "10", "11", 0)
