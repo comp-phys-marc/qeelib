7212d82a7349793a9eef5bf150b6fbfba4168247
Marcus Edwards <marcusedwards@hotmail.ca>
2019-02-19 13:01:58 -0500

    Document profiler

--- profiler.py
+++ profiler.py
@@ -32,27 +32,56 @@ def normalize_print_and_get_requirements(func):
 
 
 class Profiler:
+    """
+    A singleton class used to profile time spent during execution of a quantum program.
+    """
+
     instance = None
 
     def __init__(self):
+        """
+        Initializes the singleton if it hasn't already been initialized.
+        """
         if not Profiler.instance:
             Profiler.instance = Profiler.__Profiler()
 
     def __getattr__(self, item):
+        """
+        Attribute proxy for the underlying singleton instance.
+
+        :param item: The attribute to fetch form the singleton instance.
+        :return: The fetched attribute.
+        """
         return getattr(self.instance, item)
 
     class __Profiler:
+        """
+        A profiler instance.
+        """
 
         def __init__(self):
+            """
+            Initializes the single profiler instance for hte execution.
+            """
             self.func_profiles = {}
 
         def profile_efficiency(self, func_name, elapsed_time):
+            """
+            Registers the time spent running the provided method.
+
+            :param func_name: The method being added to the profile.
+            :param elapsed_time: The time spent running the method.
+            :return:
+            """
             if func_name not in self.func_profiles:
                 self.func_profiles[func_name] = elapsed_time
             else:
                 self.func_profiles[func_name] += elapsed_time
 
         def print(self):
+            """
+            Prints the performance profile of the execution with times spent running each method.
+            """
             print(OUTPUT_HEADER)
             for func_name in self.func_profiles:
                 print("{:15}".format(func_name), end='')
